4f14dae80ea5ed5bc11d7813f83e37bfabb73317
student <student@students-mac.student.georgianc.on.ca>
2016-02-26 09:22:43 -0500

    Initial Commit

---
+++ List of Shoppings/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  List of Shoppings
+//
+//  Created by student on 2016-02-26.
+//  Copyright © 2016 jwhiteside. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
