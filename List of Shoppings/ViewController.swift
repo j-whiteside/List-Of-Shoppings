505cd5ea454d76b59409086f6f30fb06bd1aa634
student <student@students-mac.student.georgianc.on.ca>
2016-02-26 11:39:46 -0500

    All done!

--- List of Shoppings/ViewController.swift
+++ List of Shoppings/ViewController.swift
@@ -2,7 +2,10 @@
 //  ViewController.swift
 //  List of Shoppings
 //
-//  Created by student on 2016-02-26.
+//  Created by Jeremy Whiteside (200249654) on 2016-02-26.
+//
+//  This program is a simple shopping list application.  It is a mobile app, so you can access it while at the grocery store or elsewhere.
+//
 //  Copyright © 2016 jwhiteside. All rights reserved.
 //
 
@@ -15,19 +18,17 @@ class ViewController: UIViewController {
     let LIST_ITEM_RESET_STRING = ""
     let LIST_NAME_DEFAULT_STRING = "Shopping List"
     
-    
-    @IBOutlet weak var firstListItemQuantity: UILabel!
-    @IBOutlet weak var secondListItemQuantity: UILabel!
-    @IBOutlet weak var thirdListItemQuantity: UILabel!
-    @IBOutlet weak var fourthListItemQuantity: UILabel!
-    @IBOutlet weak var fifthListItemQuantity: UILabel!
-    
     var firstListItemInt: Int = 0
     var secondListItemInt: Int = 0
     var thirdListItemInt: Int = 0
     var fourthListItemInt: Int = 0
     var fifthListItemInt: Int = 0
-    
+
+    @IBOutlet weak var firstListItemQuantity: UILabel!
+    @IBOutlet weak var secondListItemQuantity: UILabel!
+    @IBOutlet weak var thirdListItemQuantity: UILabel!
+    @IBOutlet weak var fourthListItemQuantity: UILabel!
+    @IBOutlet weak var fifthListItemQuantity: UILabel!
     
     @IBOutlet weak var listNameTextField: UITextField!
     @IBOutlet weak var firstListItemTextField: UITextField!
@@ -36,75 +37,91 @@ class ViewController: UIViewController {
     @IBOutlet weak var fourthListItemTextField: UITextField!
     @IBOutlet weak var fifthListItemTextField: UITextField!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
     @IBAction func increaseFirstItemQuantity(sender: AnyObject) {
+        //Increases the quantity of the first item
         firstListItemInt+=1
         firstListItemQuantity.text = String(firstListItemInt)
     }
-    
     @IBAction func decreaseFirstItemQuantity(sender: AnyObject) {
+        //Decreases the quantity of the first item
         firstListItemInt-=1
+        if (firstListItemInt<0){
+            firstListItemInt = 0
+        }
         firstListItemQuantity.text = String(firstListItemInt)
     }
     
     
-    
     @IBAction func increaseSecondItemQuantity(sender: AnyObject) {
-        thirdListItemInt+=1
+        //Increases the quantity of the second item
+        secondListItemInt+=1
         secondListItemQuantity.text = String(secondListItemInt)
     }
-    
     @IBAction func decreaseSecondItemQuantity(sender: AnyObject) {
+        //Decreases the quantity of the second item
         secondListItemInt-=1
+        if (secondListItemInt<0){
+            secondListItemInt = 0
+        }
         secondListItemQuantity.text = String(secondListItemInt)
     }
-    
     
     
     @IBAction func increaseThirdItemQuantity(sender: AnyObject) {
+        //Increases the quantity of the third item
         thirdListItemInt+=1
         thirdListItemQuantity.text = String(thirdListItemInt)
     }
     
     @IBAction func decreaseThirdItemQuantity(sender: AnyObject) {
+        //Decreases the quantity of the third item
         thirdListItemInt-=1
+        if (thirdListItemInt<0){
+            thirdListItemInt = 0
+        }
         thirdListItemQuantity.text = String(thirdListItemInt)
     }
     
     
-    
     @IBAction func increaseFourthItemQuantity(sender: AnyObject) {
+        //Increases the quantity of the fourth item
         fourthListItemInt+=1
         fourthListItemQuantity.text = String(fourthListItemInt)
     }
     
     @IBAction func decreaseFourthItemQuantity(sender: AnyObject) {
+        //Decreases the quantity of the fourth item
         fourthListItemInt-=1
+        if (fourthListItemInt<0){
+            fourthListItemInt = 0
+        }
         fourthListItemQuantity.text = String(fourthListItemInt)
     }
     
     
-    
     @IBAction func increaseFifthItemQuantity(sender: AnyObject) {
+        //Increases the quantity of the fifth item
         fifthListItemInt+=1
         fifthListItemQuantity.text = String(fifthListItemInt)
     }
     
     @IBAction func decreaseFifthItemQuantity(sender: AnyObject) {
+        //Decreases the quantity of the fifth item
         fifthListItemInt-=1
+        if (fifthListItemInt<0){
+            fifthListItemInt = 0
+        }
         fifthListItemQuantity.text = String(fifthListItemInt)
     }
     
     
-    
-    
-    
     @IBAction func clearAll(sender: AnyObject) {
+        //Clears all texboxes and resets the quantity to 0
         
         listNameTextField.text = LIST_NAME_DEFAULT_STRING
         
@@ -121,11 +138,6 @@ class ViewController: UIViewController {
         fifthListItemTextField.text = LIST_ITEM_RESET_STRING
         
         
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
